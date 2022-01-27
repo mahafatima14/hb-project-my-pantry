5d24b7f3ed70c19e2be73a2e19dd2ef4108cb1cf
Your Name <mahafatima14@gmail.com>
2022-01-27 18:53:06 +0000

    model.py finished and seed/crud started

--- seed.py
+++ seed.py
@@ -10,16 +10,47 @@ os.system("createdb pantry")
 model.connect_to_db(server.app)
 model.db.create_all()
 
-def load_recipes:
+def load_recipes():
     """Recipes to use for the Pantry App"""
 
     recipe_one = {
         'name': 'Chicken Soup'
-        'image_url':
+        'image_url': ''
         'instructions': 'Put the chicken, carrots, celery and onion in a large soup pot and cover with cold water. Heat and simmer, uncovered, until the chicken meat falls off of the bones (skim off foam every so often).Take everything out of the pot. Strain the broth. Pick the meat off of the bones and chop the carrots, celery and onion. Season the broth with salt, pepper and chicken bouillon to taste, if desired. Return the chicken, carrots, celery and onion to the pot, stir together, and serve.'
         'created_at': 
         'updated_at':
+        'prep time': '5 mins'
+        'cooking time': '10 mins'
+        
 
     }
     model.db.session.add_all(recipe_one)
+    model.db.session.commit()
+
+    recipe_two = {
+        'name': ''
+        'image_url':
+        'instructions': ''
+        'created_at': 
+        'updated_at':
+        'prep time': '5 mins'
+        'cooking time': '10 mins'
+        
+
+    }
+    model.db.session.add_all(recipe_two)
+    model.db.session.commit()
+
+    
+    recipe_three = {
+        'name': ''
+        'image_url':
+        'instructions': ''
+        'created_at': 
+        'updated_at':
+        'prep time': '5 mins'
+        'cooking time': '10 mins'
+    }
+
+    model.db.session.add_all(recipe_three)
     model.db.session.commit()
